d7d278e13b7302f3b5e8dd1d277d1d637c194292
Hugo José Bello Gutiérrez <hugo.bello.gu@gmail.com>
2018-07-05 18:23:32 +0200

    added helpers

--- python-test-bed-adapter/src/utils/helpers.py
+++ python-test-bed-adapter/src/utils/helpers.py
@@ -1,10 +1,19 @@
 
+import os
+
 class Helpers:
     def __init__(self):
         pass
 
     def find_files_in_dir(self,directory:str):
-        pass
+        if not os.path.isdir(directory):
+            return []
+
+        files = []
+        for filename in os.listdir(directory):
+            if ".avsc" in filename:
+                files.append(filename)
+        return files
 
     def find_missing_key_files(self,files:list):
         pass
